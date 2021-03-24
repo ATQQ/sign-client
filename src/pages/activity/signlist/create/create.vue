3f197b3eef48a5ef029f41d1b5f2a8f2c8d5c68f
ATQQ <2604395430@qq.com>
2021-03-24 11:02:34 +0800

    feat: 完成新建签到页UI修改

--- src/pages/activity/signlist/create/create.vue
+++ src/pages/activity/signlist/create/create.vue
@@ -1,24 +1,46 @@
 <template>
-  <view>
+  <view class="p20">
     <view>
-      <text>签到方式</text>
-      <checkbox-group @change="handleChange">
-        <label>
-          <checkbox :value="1" :checked="qrCode" /><text>二维码</text>
-        </label>
-        <label>
-          <checkbox :value="2" :checked="location" /><text>定位</text>
-        </label>
-      </checkbox-group>
+      <van-divider
+        contentPosition="center"
+        customStyle="color: #1989fa; border-color: #1989fa; font-size: 1rem;"
+      >
+        选择签到方式
+      </van-divider>
+      <van-checkbox-group
+        style="display: flex; justify-content: space-around"
+        :value="methods"
+        @change="handleChange"
+      >
+        <van-checkbox name="1">二维码</van-checkbox>
+        <van-checkbox name="2">定位 b</van-checkbox>
+      </van-checkbox-group>
     </view>
-    <view v-show="methods.includes(2)">
+    <view v-show="methods.includes('2')">
       <text>位置信息</text>
     </view>
-    <view class="">
-      <text>持续时长</text>
-      <input type="number" v-model="time" /> （mins）
+    <van-divider
+      contentPosition="center"
+      customStyle="color: #1989fa; border-color: #1989fa; font-size: 1rem;"
+    >
+      持续时长
+    </van-divider>
+    <picker mode="selector" :range="timeRange" @change="handleSelectorChange">
+      <view class="time">
+        <van-button size="small" type="info" round>更改</van-button>
+        <text>{{ time }} mins </text>
+      </view>
+    </picker>
+    <view class="p20">
+      <van-button
+        :disabled="!isOk"
+        size="large"
+        @click="handleCreate"
+        type="primary"
+        round
+        >确认创建</van-button
+      >
     </view>
-    <button type="default" @click="handleCreate">确认创建</button>
   </view>
 </template>
 
@@ -30,16 +52,27 @@ export default {
       methods: [],
       locationInfo: '',
       time: 5,
-      activityId: -1
+      activityId: -1,
+      timeRange: Array.from({
+        length: 60
+      }).map((_, i) => i + 1)
     }
   },
   onLoad (params) {
     const { id } = params
     this.activityId = id
   },
+  computed: {
+    isOk () {
+      return this.methods.length > 0
+    }
+  },
   methods: {
     handleChange (e) {
-      this.methods = e.detail.value.map((v) => +v)
+      this.methods = e.detail
+    },
+    handleSelectorChange (e) {
+      this.time = +e.detail.value + 1
     },
     handleCreate () {
       if (this.methods.length === 0) {
@@ -71,5 +104,10 @@ export default {
 }
 </script>
 
-<style>
+<style scoped>
+.time {
+  display: flex;
+  align-items: center;
+  justify-content: space-around;
+}
 </style>
